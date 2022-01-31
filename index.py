04634dc9f0f09943d48d8253d64a5e6fb1aeb23c
sasalilime <salimtison@gmail.com>
2022-01-31 11:43:43 +0100

    translate text to russian and write in file

--- index.py
+++ index.py
@@ -1,8 +1,14 @@
+from translate import Translator
+
+translator = Translator(to_lang="ru", from_lang="fr", encoding='utf-8')
+
 with open('file.txt', 'a') as file:
     while True:
         text = input('> ')
-        file.write(text)
         if text == 'quit':
             break
+        file.write(translator.translate(text) + '\n')
+        file.flush()
+
 
 print('bye !')
